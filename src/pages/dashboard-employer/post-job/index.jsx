02627d3fdf3b9b19ea3ab04cd3a8e52b753cba12
pngochuy <pngochuy3010@gmail.com>
2025-04-16 19:05:15 +0700

    fix deploy 8

--- src/pages/dashboard-employer/post-job/index.jsx
+++ src/pages/dashboard-employer/post-job/index.jsx
@@ -1,4 +1,4 @@
-import { JobForm } from "./jobform";
+import { JobForm } from "./JobForm";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { fetchCompanyProfile } from "@/services/employerServices";
